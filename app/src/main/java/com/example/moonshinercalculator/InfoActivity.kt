0fcc1966db0fd24e80107fdc72963559a5e3d6eb
alexey <t9521413290@yandex.ru>
2025-12-20 14:33:09 +0500

    main activity creating

--- app/src/main/java/com/example/moonshinercalculator/InfoActivity.kt
+++ app/src/main/java/com/example/moonshinercalculator/InfoActivity.kt
@@ -32,14 +32,14 @@ class InfoActivity : AppCompatActivity() {
         // Инициализация TextView для отображения информации
         val textView10 = findViewById<TextView>(R.id.textView10)
         textView10.text = buildString {
-            appendLine("Программа разработана Алексеем Уфимцевым на бесплатной основе.")
+            appendLine("Программа разработана Алексеем Уфимцевым изначально на бесплатной основе.")
             appendLine()
             appendLine("При разработке программы я воспользовался всеми формулами, находящимися в свободном доступе.")
             appendLine("а так-же формулами, которые выводил из формул в свободном доступе.")
             appendLine()
             appendLine("Однако не все эти формулы достаточно точные, собственно как и в большинстве онлайн-калькуляторов.")
             appendLine()
-            appendLine("это тестовая версия программы. 0.1")
+            appendLine("")
             appendLine("")
         }
 
